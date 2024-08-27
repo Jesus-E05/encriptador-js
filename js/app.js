4929f18c3bd4b3463edb842d3a3902a1ed7b7d9d
Jesus-E05 <152435072+Jesus-E05@users.noreply.github.com>
2024-08-27 16:53:33 -0600

    Limitando funcionamiento a minusculas solamente

--- js/app.js
+++ js/app.js
@@ -1,27 +1,27 @@
+let llaveAES = 'mi-secreto';
 
 function encriptar(){
     let textEntrada = document.getElementById('entradaTexto').value;
-    console.log(`Mostrando texto del textArea ${textEntrada}`)
-    let textoEncriptado = CryptoJS.AES.encrypt(textEntrada, 'mi-secreto').toString();
-   // document.getElementById('textoEncriptado').value = textoEncriptado;
-    console.log(`Mostrando encriptado: ${textoEncriptado}`)
+    let textoEnMinusculas =textEntrada.toLowerCase();
+    //console.log(`Mostrando texto del textArea ${textoEnMinusculas}`)
+    let textoEncriptado = CryptoJS.AES.encrypt(textoEnMinusculas, llaveAES).toString();
+    //console.log(`Mostrando encriptado: ${textoEncriptado}`)
     mostrarResultado(textoEncriptado);
 }
 
 function desencriptar(){
     let textEntrada = document.getElementById('entradaTexto').value;
-    console.log(`Mostrando texto del textArea ${textEntrada}`)
+    //console.log(`Mostrando texto del textArea ${textEntrada}`)
 
-    let bytes = CryptoJS.AES.decrypt(textEntrada, 'mi-secreto');
+    let bytes = CryptoJS.AES.decrypt(textEntrada, llaveAES);
     let textoDesencriptado = bytes.toString(CryptoJS.enc.Utf8);
-    console.log(`Mostrando desencriptado: ${textoDesencriptado}`)
+    //console.log(`Mostrando desencriptado: ${textoDesencriptado}`)
     mostrarResultado(textoDesencriptado);
 }
 
 function mostrarResultado(texto){
     let contenedorResultado = document.getElementById('resultadoEncriptador');
     
-    // Reemplaza todo el contenido del div con el resultado
     contenedorResultado.innerHTML = `
         <div class="contenedor-taResult">
             <textarea class="txt-area-resultado">${texto}</textarea>
@@ -35,7 +35,7 @@ function mostrarResultado(texto){
 function copiar(){
     let textoACopiar = document.querySelector('.txt-area-resultado');
     textoACopiar.select();
-    textoACopiar.setSelectionRange(0, 99999); // Para dispositivos móviles
+    textoACopiar.setSelectionRange(0, 99999); 
     document.execCommand("copy");
 
     alert("Texto copiado al portapapeles" );
