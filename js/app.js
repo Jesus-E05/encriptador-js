d26cbafde01ee841d20a699eb2bacf6edfef2f28
Jesus-E05 <jesus05.jesb@gmail.com>
2024-08-27 17:49:18 -0600

    Cambios para no aceptar valores nulo o en blanco, asi como agregando url al link

--- js/app.js
+++ js/app.js
@@ -2,20 +2,23 @@ let llaveAES = 'mi-secreto';
 
 function encriptar(){
     let textEntrada = document.getElementById('entradaTexto').value;
+    if (!textEntrada) {
+        alert("Por favor, ingrese un texto para encriptar.");
+        return; 
+    }
     let textoEnMinusculas =textEntrada.toLowerCase();
-    //console.log(`Mostrando texto del textArea ${textoEnMinusculas}`)
     let textoEncriptado = CryptoJS.AES.encrypt(textoEnMinusculas, llaveAES).toString();
-    //console.log(`Mostrando encriptado: ${textoEncriptado}`)
     mostrarResultado(textoEncriptado);
 }
 
 function desencriptar(){
     let textEntrada = document.getElementById('entradaTexto').value;
-    //console.log(`Mostrando texto del textArea ${textEntrada}`)
-
+    if (!textEntrada) {
+        alert("Por favor, ingrese un texto para desencriptar.");
+        return; 
+    }
     let bytes = CryptoJS.AES.decrypt(textEntrada, llaveAES);
     let textoDesencriptado = bytes.toString(CryptoJS.enc.Utf8);
-    //console.log(`Mostrando desencriptado: ${textoDesencriptado}`)
     mostrarResultado(textoDesencriptado);
 }
 
